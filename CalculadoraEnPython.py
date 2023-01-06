9b671b98b7615fde22e0d2001d20ab475fa4ee84
sasamue <murillo.samuel.2735@eam.edu.co>
2023-01-06 12:39:55 -0500

    calculadora en python

---
+++ CalculadoraEnPython.py
@@ -0,0 +1,29 @@
+# Vamos a crear una calculadora con operaciones matematicas basicas
+valor1 = 7 
+valor2 = 4 
+
+resultado = valor1 + valor2
+resultado2 = valor1 - valor2
+resultado3 = valor1 / valor2
+resultado4 = valor1 * valor2
+
+print("el resultado de la suma es:",resultado)
+print("el resultado de la resta es:",resultado2)
+print("el resultado de la multiplicacion es:",resultado4)
+print("el resultado de la division es:",resultado3)
+
+a= input("ingrese el primer numero:")
+num1 = int(a)
+b= input("ingrese el segundo numero:")
+num2 = int(b)
+
+
+resultado5 = num1 + num2
+resultado6 = num1 - num2
+resultado7 = num1 / num2
+resultado8 = num1 * num2
+
+print("el resultado de la suma es:",resultado5)
+print("el resultado de la resta es:",resultado6)
+print("el resultado de la multiplicacion es:",resultado8)
+print("el resultado de la division es:",resultado7)
